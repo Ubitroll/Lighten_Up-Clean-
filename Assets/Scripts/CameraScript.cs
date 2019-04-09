769bd87a7cc69a7f15afc4933f801da79ea17f09
dejwkubikson <44845403+dejwkubikson@users.noreply.github.com>
2019-04-09 18:31:05 +0100

    Created scene for trailer

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -7,17 +7,23 @@ public class CameraScript : MonoBehaviour
 
     public float moveToZ;
     public float cameraSpeed;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 target = new Vector3(transform.position.x, transform.position.y, moveToZ);
-        Vector3 lerpedPos = Vector3.Lerp(transform.position, target, cameraSpeed * cameraSpeed);
-        transform.position = lerpedPos;
+        timer += Time.deltaTime;
+
+        if (timer > 3)
+        {
+            Vector3 target = new Vector3(transform.position.x, transform.position.y, moveToZ);
+            Vector3 lerpedPos = Vector3.Lerp(transform.position, target, (1 - cameraSpeed) * cameraSpeed);
+            transform.position = lerpedPos;
+        }
     }
 }
